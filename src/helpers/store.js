e7c1986fc0f39465e92cb5dc46a081afcc574457
lucas <lucas.m.maris@gmail.com>
2024-08-18 12:51:38 +0300

    [ fix ] - fixed issue where transparent walls would be converted in solid after deletion

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -1,7 +1,7 @@
 import create from 'zustand'
 
 const useStore = create((set, get) => ({
-  viewMode: '3D', // Default to 3D view
+  viewMode: '2D', // Default to 3D view
   cameraSettings: {
     '3D': { position: [0, 5, 10], fov: 50 },
     '2D': { position: [0, 100, 0], zoom: 100 },
@@ -17,12 +17,20 @@ const useStore = create((set, get) => ({
       },
     })),
 
-  walls: [], // Array to store walls in the scene
+  walls: [],
+  addWall: (wall) =>
+    set((state) => ({
+      walls: [...state.walls, { ...wall, type: wall.type || 'solid' }],
+    })),
 
-  addWall: (wall) => set((state) => ({ walls: [...state.walls, wall] })),
   removeWall: (index) =>
     set((state) => ({
       walls: state.walls.filter((_, i) => i !== index),
+    })),
+
+  updateWallType: (index, newType) =>
+    set((state) => ({
+      walls: state.walls.map((wall, i) => (i === index ? { ...wall, type: newType } : wall)),
     })),
 }))
 
